0019c3946d91188625c088bef5cc9c6dd475a425
aqilrahmat3 <144086802+aqilrahmat3@users.noreply.github.com>
2023-10-10 17:23:00 +0700

    Add files via upload

---
+++ Pertemuan 7 DASPRO/While05.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class While05 {
+    public static void main(String[] args) {
+        
+    Scanner sc = new Scanner(System.in);  
+    int i = 1;
+    while (i <= 10) {
+        if (i % 2 == 0) {
+            System.out.println("Angka " + i + " adalah bilangan genap");
+        }else {
+            System.out.println("Angka " + i + " adalah bilangan ganjil");
+        }
+        i++;
+    }
+
+    
+    }
+}
